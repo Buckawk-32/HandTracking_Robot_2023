18abb5f314a0652cc18ca1906c4c24a157d4ce3b
Buckawk-32 <117026619+Buckawk-32@users.noreply.github.com>
2023-06-04 18:15:12 +0800

    Finished with the Hand Tracking
    Hand Tracking that will be implemented with a robot hand later on

--- HandTrackingRobot_PythonVenv/HandTracking.py
+++ HandTrackingRobot_PythonVenv/HandTracking.py
@@ -3,31 +3,59 @@ import mediapipe.python.solutions.hands as mp_hands
 import mediapipe.python.solutions.drawing_utils as mp_drawingutils
 import mediapipe.python.solutions.drawing_styles as mp_drawingstyles
 
-cap = cv2.VideoCapture(1  )
+from Func import findposition
+
+cap = cv2.VideoCapture(1)
 fourcc = cv2.VideoWriter_fourcc('m','p', '4', 'v')
 
-with mp_hands.Hands(static_image_mode=False, max_num_hands=1, model_complexity=1, min_tracking_confidence=0.7, min_detection_confidence=0.7) as hands:
+Hand_Face = False
+Index_Finger = False
+Middle_Finger = False
+Ring_Finger = False
+Pinky_Finger = False
+
+with mp_hands.Hands(static_image_mode=False, max_num_hands=1 , model_complexity=1, min_tracking_confidence=0.7, min_detection_confidence=0.7) as hands:
     while True:
         ret, frame = cap.read()
+        if not ret:
+            print("Ignore")
+            continue
 
         frame1 = cv2.resize(frame, (640, 480))
 
         frame2 = cv2.flip(frame1, 1)
+        
+        findpos = findposition(frame2)
+        if len(findpos)!=0:
+            if findpos[0][1:] < findpos[4][1:]:
+                Hand_Face = True
+            else:
+                Hand_Face = False
 
-        results = hands.process(cv2.cvtColor(frame2, cv2.COLOR_BGR2RGB))
+            if findpos[8][2:] > findpos[6][2:]:
+                Index_Finger = True
+            else:
+                Index_Finger = False
 
-        if results.multi_hand_landmarks != None:
-            for multi_hand_landmarks in results.multi_hand_landmarks:
-                mp_drawingutils.draw_landmarks(
-                    frame2,
-                    multi_hand_landmarks,
-                    mp_hands.HAND_CONNECTIONS)
+            if findpos[12][2:] > findpos[10][2:]:
+                Middle_Finger = True
+            else:
+                Middle_Finger = False
 
-        cv2.imshow("HandTracking Window", frame2)
+            if findpos[16][2:] > findpos[14][2:]:
+                Ring_Finger = True
+            else:
+                Ring_Finger = False
+
+            if findpos[18][2:] > findpos[16][2:]:
+                Pinky_Finger = True
+            else:
+                Pinky_Finger = False
+    
+
+        cv2.imshow("Hand Tracking", frame2)
 
         waitkey = cv2.waitKey(1)
 
         if waitkey == ord("q"):
             break
-
-    cap.release()
